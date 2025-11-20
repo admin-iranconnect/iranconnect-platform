65fa60bce4e01c65982dcffaad7ca474f15f4c14
admin-iranconnect <admin@iranconnect.org>
2025-11-21 00:34:16 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE, // 🔹 این متغیر درست است
+  database: process.env.DB_NAME, // 🔹 این متغیر درست است
   ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false, // ✅ برای Neon لازم است
 });
 
