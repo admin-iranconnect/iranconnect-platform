4fc7b8bd9c2498798966988f1751de8072de6b90
admin-iranconnect <admin@iranconnect.org>
2025-11-22 20:37:23 +0100

    Update apiClient.js

--- frontend/utils/apiClient.js
+++ frontend/utils/apiClient.js
@@ -6,7 +6,7 @@ const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000";
 
 // یک نمونه اختصاصی axios می‌سازیم
 const apiClient = axios.create({
-  baseURL: `${API_BASE},
+  baseURL: `${API_BASE}/api`,
   withCredentials: true,
 });
 
